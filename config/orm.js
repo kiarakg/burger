4c33bb47cb842f3a6a8e16873b0b808a533e90ac
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-18 16:11:31 -0400

    handlebars updated

--- config/orm.js
+++ config/orm.js
@@ -2,27 +2,27 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function(tableInput) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     };
 
-    insertOne: function(tableInput, columnName, burgerName) {
+    insertOne: function(tableInput, columnName, burgerName, cb) {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [tableInput, columnName, burgerName], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
 
-    updateOne: function(tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal) {
+    updateOne: function(tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal, cb) {
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString [tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     }
 };
