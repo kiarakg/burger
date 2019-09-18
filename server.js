0a6b334b49e5b8bd35bd07f7fa0fde1e352de508
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-18 15:06:55 -0400

    db created and port listening

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start server
+app.listen(PORT, function() {
+    console.log("Listening on PORT " + PORT);
+});
