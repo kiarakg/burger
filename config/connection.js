4372bf36e32a83a3004315d120aa72fd6aafcdda
Kiara Guzman <kiara.kg11@gmail.com>
2019-11-01 01:20:01 -0400

    connection.js updated

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
+// Require mysql npm package to create a connection to the mysql database.
 var mysql = require("mysql");
 
+require("dotenv").config();
 
-let connection;
-
+// Define database connection properties
+// Use production database when deployed.
 if (process.env.JAWSDB_URL) {
+  // Heroku deployment
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
+
 else {
-  connection = mysql.createConnection({
+  // else use localhost database for local development.
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -16,4 +21,15 @@ else {
   });
 }
 
+connection.connect(function(err) {
+  // If there is an error when connecting to the database, log the error to the console.
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  // If a database connection is established, log the database thread number.
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection properties
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-//Dependencies
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 
-//Define port the server will be listening on.
+// Define port the server will be listening on.
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
-//Parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Parse application/json
+// Parse application/json
 app.use(bodyParser.json());
 
-//Set Handlebars.
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -27,7 +27,7 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-//App is listening...
+// Listener
 app.listen(PORT, function() {
   console.log("App now listening at http://localhost:" + PORT);
 });
